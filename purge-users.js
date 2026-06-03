69cbec22b4917c18cb80d6459bbd1448844a164c
sherifmohammed000 <thesheriff258@gmail.com>
2026-06-03 23:53:59 +0000

    purged users

--- purge-users.js
+++ purge-users.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account.json (2).json");
+const serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
